d841312e62783ba3177c9e26e861217e71351007
sarhani saad <saad1996kenitra@gmail.com>
2021-02-21 20:48:50 +0100

    first commit

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let token = "EAANJgBYQmhcBAL0IsuyXQCWvRLpNkBTEZCfip6GuwjkZAW7tyctyTcQ6553ZC8TPezCZAVNOtxbNBRdZAZCuCj7uR87od9tfpdfYrZBZC9Qh4O7N9N4SloYi3Lp0WqI4ChZAvV0QxE1qjQAEjRBeqeIZBkhwlCp0A1FRWJzbAGgygLagZDZD"
+let token = "your token page"
 app.get('/', function (req, res) {
     res.send('hello world')
 })
 
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'sjsxqs76hgfhg5454gfdgfdJ') {
+    if (req.query['hub.verify_token'] === 'your token') {
         res.send(req.query['hub.challenge'])
     }
     res.send('token non valid');
